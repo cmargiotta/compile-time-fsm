bfd7d66bcc9f3be2276a4c3f9e28c4afbca485b4
Carmine Margiotta <car.margiotta@icloud.com>
2022-08-08 22:22:03 +0200

    Fixed namespace starting with __

--- src/fsm/fsm.hpp
+++ src/fsm/fsm.hpp
@@ -24,97 +24,6 @@ MAKE_EXISTENCE_VERIFIER(on_exit)
 MAKE_EXISTENCE_VERIFIER(on_transit)
 MAKE_EXISTENCE_VERIFIER(id)
 
-namespace __ctfsm__
-{
-    template<class states, class set = std::tuple<>>
-    struct state_expander;
-
-    template<class... set>
-    struct state_expander<std::tuple<>, std::tuple<set...>>
-    {
-            // Base case, there are no more nodes to expand. This handles eventual cycles in the
-            // graph too.
-        public:
-            using states = std::tuple<set...>;
-    };
-
-    template<class state, class set>
-    struct state_expander<std::tuple<state>, set>
-    {
-            // Expand a single state and every reachable state from that
-        public:
-            // Adding State and the reachable nodes from it to set
-            using state_set
-                = ctfsm::type_set_merge<typename ctfsm::extract_values<typename state::transitions>::values,
-                                        typename ctfsm::type_set_merge<std::tuple<state>, set>::set>;
-
-            // Expanding the nodes that were not already in set
-            using states =
-                typename state_expander<typename state_set::delta, typename state_set::set>::states;
-    };
-
-    template<class state, class... states_, class set>
-    struct state_expander<std::tuple<state, states_...>, set>
-    {
-            // Iterate through the states list and expand every state
-        private:
-            using state_set
-                = ctfsm::type_set_merge<typename ctfsm::extract_values<typename state::transitions>::Values,
-                                        typename ctfsm::type_set_merge<std::tuple<state>, set>::set>;
-
-        public:
-            using states =
-                typename ctfsm::type_set_merge<typename state_expander<std::tuple<state>, set>::states,
-                                               typename state_expander<std::tuple<states_...>, set>::states>::set;
-    };
-
-    template<class elements>
-    struct variant_builder;
-
-    template<class... elements>
-    struct variant_builder<std::tuple<elements...>>
-    {
-        public:
-            using variant = std::variant<elements*...>;
-    };
-
-    template<class... elements>
-    using variant_builder_t = typename variant_builder<elements...>::variant;
-
-    template<class states>
-    struct id_types_verifier;
-
-    template<ctfsm::has_id state, ctfsm::has_id... states>
-    struct id_types_verifier<std::tuple<state, states...>>
-    {
-        public:
-            static_assert(std::conjunction_v<std::is_same<decltype(state::id), decltype(states::id)>...>,
-                          "Every state ID is of the same type");
-
-            using type = typename std::remove_cv<decltype(state::id)>::type;
-    };
-
-    template<class state, class... states>
-    struct id_types_verifier<std::tuple<state, states...>>
-    {
-        public:
-            using type = decltype(typeid(state).name());
-    };
-
-    template<class state>
-    struct id_extractor
-    {
-            static const inline auto id = typeid(state).name();
-    };
-
-    template<ctfsm::has_id state>
-    struct id_extractor<state>
-    {
-            static constexpr auto id = state::id;
-    };
-
-}// namespace __ctfsm__
-
 namespace ctfsm
 {
     /**
@@ -123,16 +32,105 @@ namespace ctfsm
      *
      * @tparam state
      */
-    template<class state>
+    template<class initial_state>
     class fsm
     {
-        public:
-            using states  = typename __ctfsm__::state_expander<std::tuple<state>>::states;
-            using id_type = typename __ctfsm__::id_types_verifier<states>::type;
+        private:
+            template<class states, class set = std::tuple<>>
+            struct state_expander;
 
-            states                               _states;
-            __ctfsm__::variant_builder_t<states> _current_state;
-            const id_type*                       _current_state_id;
+            template<class... set>
+            struct state_expander<std::tuple<>, std::tuple<set...>>
+            {
+                    // Base case, there are no more nodes to expand. This handles eventual cycles in
+                    // the graph too.
+                public:
+                    using states = std::tuple<set...>;
+            };
+
+            template<class state, class set>
+            struct state_expander<std::tuple<state>, set>
+            {
+                    // Expand a single state and every reachable state from that
+                public:
+                    // Adding State and the reachable nodes from it to set
+                    using state_set = ctfsm::type_set_merge<
+                        typename ctfsm::extract_values<typename state::transitions>::values,
+                        typename ctfsm::type_set_merge<std::tuple<state>, set>::set>;
+
+                    // Expanding the nodes that were not already in set
+                    using states =
+                        typename state_expander<typename state_set::delta, typename state_set::set>::states;
+            };
+
+            template<class state, class... states_, class set>
+            struct state_expander<std::tuple<state, states_...>, set>
+            {
+                    // Iterate through the states list and expand every state
+                private:
+                    using state_set = ctfsm::type_set_merge<
+                        typename ctfsm::extract_values<typename state::transitions>::Values,
+                        typename ctfsm::type_set_merge<std::tuple<state>, set>::set>;
+
+                public:
+                    using states = typename ctfsm::type_set_merge<
+                        typename state_expander<std::tuple<state>, set>::states,
+                        typename state_expander<std::tuple<states_...>, set>::states>::set;
+            };
+
+            template<class elements>
+            struct variant_builder;
+
+            template<class... elements>
+            struct variant_builder<std::tuple<elements...>>
+            {
+                public:
+                    using variant = std::variant<elements*...>;
+            };
+
+            template<class... elements>
+            using variant_builder_t = typename variant_builder<elements...>::variant;
+
+            template<class states>
+            struct id_types_verifier;
+
+            template<ctfsm::has_id state, ctfsm::has_id... states>
+            struct id_types_verifier<std::tuple<state, states...>>
+            {
+                public:
+                    static_assert(
+                        std::conjunction_v<std::is_same<decltype(state::id), decltype(states::id)>...>,
+                        "Every state ID is of the same type");
+
+                    using type = typename std::remove_cv<decltype(state::id)>::type;
+            };
+
+            template<class state, class... states>
+            struct id_types_verifier<std::tuple<state, states...>>
+            {
+                public:
+                    using type = decltype(typeid(state).name());
+            };
+
+            template<class state>
+            struct id_extractor
+            {
+                    static const inline auto id = typeid(state).name();
+            };
+
+            template<ctfsm::has_id state>
+            struct id_extractor<state>
+            {
+                    static constexpr auto id = state::id;
+            };
+
+        public:
+            using states  = typename state_expander<std::tuple<initial_state>>::states;
+            using id_type = typename id_types_verifier<states>::type;
+
+            states                    _states;
+            variant_builder_t<states> _current_state;
+            const id_type*            _current_state_id;
 
         private:
             template<class current_state, class _event>
@@ -187,7 +185,7 @@ namespace ctfsm
                 invoke_on_transit(event);
 
                 _current_state    = &std::get<target_state>(_states);
-                _current_state_id = &__ctfsm__::id_extractor<target_state>::id;
+                _current_state_id = &id_extractor<target_state>::id;
 
                 invoke_on_enter(std::get<target_state>(_states), event);
             }
@@ -198,8 +196,8 @@ namespace ctfsm
              * state as current
              */
             constexpr fsm() noexcept
-                : _current_state(&std::get<state>(_states)),
-                  _current_state_id(&__ctfsm__::id_extractor<state>::id)
+                : _current_state(&std::get<initial_state>(_states)),
+                  _current_state_id(&id_extractor<initial_state>::id)
             {
             }
 
