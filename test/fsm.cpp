56c218109d5115f14b4c8ad2180cda09f637cc1e
Carmine Margiotta <cmargiotta@posteo.net>
2023-12-04 10:03:08 +0100

    Tested on_enter without arguments

--- test/fsm.cpp
+++ test/fsm.cpp
@@ -21,6 +21,10 @@ struct switch_off
 {
 };
 
+struct force
+{
+};
+
 struct state_off;
 struct state_on;
 
@@ -32,10 +36,16 @@ struct state_on
         static constexpr std::string_view id {"ON"};
         static constinit inline bool      switched_off = false;
         static constinit inline bool      on_entered   = false;
+        static constinit inline bool      forced       = false;
 
         void on_enter(switch_on&)
         {
             on_entered = true;
+        }
+
+        void on_enter()
+        {
+            forced = true;
         }
 
         void on_exit()
@@ -46,7 +56,8 @@ struct state_on
 
 struct state_off
 {
-        using transitions = ctfsm::type_map<std::pair<switch_on, state_on>>;
+        using transitions
+            = ctfsm::type_map<std::pair<switch_on, state_on>, std::pair<force, state_on>>;
 
         static constexpr std::string_view id {"OFF"};
         static constinit inline bool      entered = false;
@@ -92,6 +103,15 @@ TEST_CASE("FSM basic usage", "[fsm]")
 
     fsm(on);
     REQUIRE(fsm.get_current_state_id() == "ON");
+    REQUIRE(fsm.is_current_state<state_on>());
+
+    fsm(switch_off());
+    REQUIRE(fsm.get_current_state_id() == "OFF");
+    REQUIRE(fsm.is_current_state<state_off>());
+
+    fsm(force {});
+    REQUIRE(fsm.get_current_state_id() == "ON");
+    REQUIRE(state_on::forced);
     REQUIRE(fsm.is_current_state<state_on>());
 
     auto current_id = fsm.invoke_on_current([](auto&& current, auto& fsm) { return current.id; });
